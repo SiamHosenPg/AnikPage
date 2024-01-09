8148f33e6fbca4d935792fa0c455d1b8210a9c4c
Shium Hossen <112849781+SiamHosenPg@users.noreply.github.com>
2024-01-09 12:33:04 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function MenuClick(MnuClk){
-
     var MenuPostiionJs = document.querySelector('.Header .MeluList');
-
     MenuPostiionJs.classList.toggle('TogglMeluList') ;
+}
+// this for Protfleo click 
+function PfClick(ProtfleoFrem){
+    
+    var img =  ProtfleoFrem.children[0];
+    //document.getElementById('hhhhh').innerHTML=img.src;
+
 }
